962e435446b1773908f0a94d80e5b35f24fc1c38
★ZAK★ <nikitafisenko717@gmail.com>
2023-11-20 18:58:21 +0300

    feat: updated security configuration

--- src/main/java/project/web/config/SecurityConfiguration.java
+++ src/main/java/project/web/config/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     final CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedOriginPatterns(Arrays.asList("*"));
     configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
